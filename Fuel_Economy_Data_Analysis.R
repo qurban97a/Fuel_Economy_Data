f3d08c48d315d2d0cc89f74a64936ee530074998
qurban97a <qurban.huseynli97@mail.ru>
2021-05-03 18:03:27 +0400

    R

---
+++ Fuel_Economy_Data_Analysis.R
@@ -0,0 +1,169 @@
+library(tidyverse)
+library(data.table)
+library(rstudioapi)
+library(skimr)
+library(inspectdf)
+library(mice)
+library(plotly)
+library(highcharter)
+library(recipes) 
+library(caret) 
+library(purrr) 
+library(graphics) 
+library(Hmisc) 
+library(glue)
+library(h2o) 
+library(ggplot2)
+h2o.init()
+
+
+#1
+
+raw <- mpg
+raw %>% view()
+raw %>% skim()
+
+
+ggplot(mpg, aes(displ, hwy, colour = class)) + 
+  geom_point()
+
+
+#2
+
+raw %>% skim()
+raw %>% view()
+
+
+
+raw %>% inspect_na()
+
+
+
+df.num <- raw %>%
+  select_if(is.numeric) %>%
+  select(cty,everything())
+
+df.chr <- raw %>%
+  select_if(is.character)
+
+
+df.chr <- dummyVars(" ~ .", data = df.chr) %>% 
+  predict(newdata = df.chr) %>% 
+  as.data.frame()
+df <- cbind(df.chr,df.num) %>%
+  select(cty,everything())
+
+names(df) <- names(df) %>% 
+  str_replace_all(" ","_") %>%
+  str_replace_all("-","_") %>%
+  str_replace_all("\\(","") %>% 
+  str_replace_all("\\)","") %>% 
+  str_replace_all("\\'","")
+df.chr %>% view()
+
+#Multicollinearity
+
+
+
+target <- 'cty'
+features <- df %>% select(-cty) %>% names()
+f <- as.formula(paste(target, paste(features, collapse = " + "), sep = " ~ "))
+glm <- glm(f, data = df)
+
+glm %>% summary()
+
+coef_na <- attributes(alias(glm)$Complete)$dimnames[[1]]
+features <- features[!features %in% coef_na]
+
+f <- as.formula(paste(target, paste(features, collapse = " + "), sep = " ~ "))
+glm <- glm(f, data = df)
+
+glm %>% summary()
+
+
+while(glm %>% faraway::vif() %>% sort(decreasing = T) %>% .[1] >= 1.5){
+  afterVIF <- glm %>% faraway::vif() %>% sort(decreasing = T) %>% .[-1] %>% names()
+  f <- as.formula(paste(target, paste(afterVIF, collapse = " + "), sep = " ~ "))
+  glm <- glm(f, data = df)
+}
+
+glm %>% faraway::vif() %>% sort(decreasing = T) %>% names() -> features 
+
+df <- df %>% select(cty,features)
+
+df %>% view()
+
+df %>% glimpse()
+
+df[,-1] <- df[,-1] %>% scale() %>% as.data.frame()
+
+
+
+#3. Fit Generalized Linear Model using H2O in R.
+
+h2o.init()
+h2o_data <- df %>% as.h2o()
+
+
+h2o_data <- h2o_data %>% h2o.splitFrame(ratios = 0.8, seed = 123)
+train <- h2o_data[[1]]
+test <- h2o_data[[2]]
+
+
+target <- 'cty'
+features <- df %>% select(-cty) %>% names()
+
+
+
+model <- h2o.glm(
+  x = features, y = target,
+  training_frame = train,
+  validation_frame = test,
+  nfolds = 10, seed = 123,
+  lambda = 0, compute_p_values = T )
+
+model@model$coefficients_table %>%
+  as.data.frame() %>%
+  dplyr::select(names,p_value) %>%
+  mutate(p_value = round(p_value,3)) %>%
+  .[-1,] %>%
+  arrange(desc(p_value))
+
+
+
+
+while(model@model$coefficients_table %>%
+      as.data.frame() %>%
+      dplyr::select(names,p_value) %>%
+      mutate(p_value = round(p_value,3)) %>%
+      .[-1,] %>%
+      arrange(desc(p_value)) %>%
+      .[1,2] > 0.05) {
+  model@model$coefficients_table %>%
+    as.data.frame() %>%
+    dplyr::select(names,p_value) %>%
+    mutate(p_value = round(p_value,3)) %>%
+    filter(!is.nan(p_value)) %>%
+    .[-1,] %>%
+    arrange(desc(p_value)) %>%
+    .[1,1] -> v
+  features <- features[features!=v]
+  
+  train_h2o <- train %>% as.data.frame() %>% select(target,features) %>% as.h2o()
+  test_h2o <- test %>% as.data.frame() %>% select(target,features) %>% as.h2o()
+  
+  model <- h2o.glm(
+    x = features, y = target,
+    training_frame = train,
+    validation_frame = test,
+    nfolds = 10, seed = 123,
+    lambda = 0, compute_p_values = T)
+}
+model@model$coefficients_table %>%
+  as.data.frame() %>%
+  dplyr::select(names,p_value) %>%
+  mutate(p_value = round(p_value,3)) 
+
+
+y_pred <- model %>% h2o.predict(newdata = test) %>% as.data.frame()
+y_pred$predict
